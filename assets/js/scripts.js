726ce2af4396b6fc10bab867d2f5d43dde4debeb
Gustavo <padilhadeoliveiragustavo@gmail.com>
2021-06-05 18:06:16 -0300

    fix: funcao loadFile

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -193,3 +193,13 @@ function raster(){
   win.document.open();
   win.document.write(iframe);
 }
+function loadFile(filePath) {
+  var result = null;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", filePath, false);
+  xmlhttp.send();
+  if (xmlhttp.status==200) {
+    result = xmlhttp.responseText;
+  }
+  return result;
+}
