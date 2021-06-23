56a641427a13730c17d0ab0a9a212bde2fc9ad5c
Bernardo Camargo <bernardopcamargo@gmail.com>
2021-06-23 00:55:52 -0300

    wip

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,6 +5,7 @@ var ambientLight, pointLight;
 var controls;
 var inicialScale = 1;
 var object = 0;
+var cube1, cube2;
 
 window.addEventListener("keydown", onKeyDown, false);
 //Init Program
@@ -15,6 +16,19 @@ animate();
 function init() {
   // Scene
   scene = new THREE.Scene();
+  loader = new THREE.TextureLoader();
+  loader.setCrossOrigin("");
+
+  bgTexture = loader.load(
+    "https://images.pexels.com/photos/1205301/pexels-photo-1205301.jpeg",
+    function (texture) {
+      var img = texture.image;
+      bgWidth = img.width;
+      bgHeight = img.height;
+      resize();
+    }
+  );
+  scene.background = bgTexture;
 
   //Camera
   camera = new THREE.PerspectiveCamera(
@@ -33,33 +47,71 @@ function init() {
   camera.add(pointLight);
 
   //Axis for help
-  var axis = new THREE.AxisHelper(10);
-  scene.add(axis);
+  // var axis = new THREE.AxisHelper(10);
+  // scene.add(axis);
 
   //Load Models
   THREE.Loader.Handlers.add(/\.dds$/i, new THREE.DDSLoader());
 
-  //Load homan male
+  var phongShader = THREE.ShaderLib.phong;
+  var mUniforms = THREE.UniformsUtils.clone(phongShader.uniforms);
+
+  // Material 1
+  material = new THREE.ShaderMaterial({
+    uniforms: mUniforms,
+    vertexShader: document.getElementById("vertexShader").textContent,
+    fragmentShader: document.getElementById("fragmentShader").textContent,
+  });
+
+  // CUBE 1
+  var geometry = new THREE.BoxBufferGeometry(4, 4, 4);
+  cube1 = new THREE.Mesh(geometry, material);
+  scene.add(cube1);
+  cube1.position.set(5, 4, -10);
+  cube1.rotation.y = 0;
+
+  // Material 2
+  material = new THREE.RawShaderMaterial({
+    uniforms: {
+      colorA: { type: "vec3", value: new THREE.Color(0xff0000) },
+      colorB: { type: "vec3", value: new THREE.Color(0x0000ff) },
+    },
+    vertexShader: vertexShader(),
+    fragmentShader: fragmentShader(),
+  });
+
+  // CUBE 2
+  var geometry = new THREE.BoxBufferGeometry(2, 4, 2);
+  cube2 = new THREE.Mesh(geometry, material);
+  scene.add(cube2);
+  cube2.position.set(0, 5, 10);
+  cube2.rotation.y = 0;
+
+  //Load hulk abomination
   new THREE.MTLLoader()
-    .setPath("assets/3D/base_spider_man/")
-    .load("base_spider_man.mtl", function (materials) {
+    .setPath("assets/3D/hulk-abomination/")
+    .load("hulk.mtl", function (materials) {
       materials.preload();
       new THREE.OBJLoader()
         .setMaterials(materials)
-        .setPath("assets/3D/base_spider_man/")
-        .load("base_spider_man.obj", function (object) {
-          spiderman = object;
-          scene.add(spiderman);
-          spiderman.position["x"] = 0;
-          spiderman.position["y"] = 0;
-          spiderman.position["z"] = 0;
-          spiderman.rotation.y = 0;
+        .setPath("assets/3D/hulk-abomination/")
+        .load("hulk.obj", function (object) {
+          var scale = 0.007;
+          hulkAbomination = object;
+          scene.add(hulkAbomination);
+          hulkAbomination.position["x"] = 0;
+          hulkAbomination.position["y"] = 0;
+          hulkAbomination.position["z"] = 0;
+          hulkAbomination.rotation.y = 0;
+          hulkAbomination.scale["x"] = scale;
+          hulkAbomination.scale["y"] = scale;
+          hulkAbomination.scale["z"] = scale;
 
-          console.log(spiderman.matrix);
+          console.log(hulkAbomination.matrix);
         });
     });
 
-    new THREE.MTLLoader()
+  new THREE.MTLLoader()
     .setPath("assets/3D/doctor_octopus/")
     .load("octopus.mtl", function (materials) {
       materials.preload();
@@ -67,34 +119,58 @@ function init() {
         .setMaterials(materials)
         .setPath("assets/3D/doctor_octopus/")
         .load("octopus.obj", function (object) {
+          var scale = 3;
           octopus = object;
           scene.add(octopus);
-          octopus.position["x"] = 0;
-          octopus.position["y"] = 0;
-          octopus.position["z"] = 5;
-          octopus.rotation.y = -2;
-          octopus.scale.set(2,2,2)
+          octopus.position["x"] = -10;
+          octopus.position["y"] = 4;
+          octopus.position["z"] = 3;
+          octopus.scale["x"] = scale;
+          octopus.scale["y"] = scale;
+          octopus.scale["z"] = scale;
+          octopus.rotation.y = -3.2;
 
           console.log(octopus.matrix);
         });
     });
 
+  new THREE.MTLLoader()
+    .setPath("assets/3D/ironman/")
+    .load("Mark 42.mtl", function (materials) {
+      materials.preload();
+      new THREE.OBJLoader()
+        .setMaterials(materials)
+        .setPath("assets/3D/ironman/")
+        .load("Mark 42.obj", function (object) {
+          var scale = 3;
+          ironman = object;
+          scene.add(ironman);
+          ironman.position["x"] = -5;
+          ironman.position["y"] = 0;
+          ironman.position["z"] = -10;
+          ironman.scale["x"] = scale;
+          ironman.scale["y"] = scale;
+          ironman.scale["z"] = scale;
+          ironman.rotation.y = 0.5;
 
-    //Esfera com tonalização Phong
-    var geometrySphere = new THREE.SphereBufferGeometry(2, 40, 40);
-    var materialSphere = new THREE.MeshPhongMaterial( {color: 0x191970} );
-    sphere = new THREE.Mesh(geometrySphere, materialSphere);
-    sphere.position.set(5,0,0);
-    scene.add(sphere);
+          console.log(ironman.matrix);
+        });
+    });
 
+  //Esfera com tonalização Phong
+  var geometrySphere = new THREE.SphereBufferGeometry(2, 40, 40);
+  var materialSphere = new THREE.MeshPhongMaterial({ color: 0x191970 });
+  sphere = new THREE.Mesh(geometrySphere, materialSphere);
+  sphere.position.set(5, 0, 0);
+  scene.add(sphere);
 
-    const light = new THREE.PointLight( 0xffff00, 1, 100 );
-    light.position.set( 5, 5, 5 );
-    scene.add( light );
+  const light = new THREE.PointLight(0xffff00, 1, 100);
+  light.position.set(5, 5, 5);
+  scene.add(light);
 
   // Create Render
   renderer = new THREE.WebGLRenderer();
-  renderer.setClearColor("#000");
+  // renderer.setClearColor("#333");
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
@@ -106,19 +182,35 @@ function init() {
 
 function animate() {
   requestAnimationFrame(animate);
+  rotate();
   controls.update();
   renderer.render(scene, camera);
 }
 
-function openImg() { //Exibindo objeto 3D em uma Imagem
-  var tab = window.open('', ''); //Abrindo nova guia
+function openImg() {
+  //Exibindo objeto 3D em uma Imagem
+  var tab = window.open("", ""); //Abrindo nova guia
   tab.document.title = "Visualização em Imagem"; //Inserindo um título na nova guia
-  
+
   //Criando imagem e inserindo ela no DOM da nova aba
   var img = new Image();
   renderer.render(scene, camera);
   img.src = renderer.domElement.toDataURL();
-  tab.document.body.appendChild(img);    
+  tab.document.body.appendChild(img);
+}
+
+function rotate() {
+  var speed = 0.01;
+  if (cube1 != null) {
+    cube1.rotation.x += speed;
+    cube1.rotation.y += speed * 2;
+    cube1.rotation.z += speed;
+  }
+  if (cube2 != null) {
+    cube2.rotation.x += speed;
+    cube2.rotation.y += speed * 2;
+    cube2.rotation.z += speed;
+  }
 }
 
 // Buttom Controls
@@ -134,27 +226,24 @@ function onKeyDown(e) {
   switch (e.which) {
     //Mover o spidermen para frente
     case 84:
-      if(object == 0){
-        object = 1
-      }
-      else{
-        object = 0
+      if (object == 0) {
+        object = 1;
+      } else {
+        object = 0;
       }
       break;
     case 38:
-      if(object == 0){
-        spiderman.position["z"] += stepPosition;
-      }
-      else{
+      if (object == 0) {
+        hulkAbomination.position["z"] += stepPosition;
+      } else {
         octopus.position["z"] += stepPosition;
       }
       break;
     //Mover o spidermen para tras
     case 40:
-      if(object == 0){
-        spiderman.position["z"] -= stepPosition;
-      }
-      else{
+      if (object == 0) {
+        hulkAbomination.position["z"] -= stepPosition;
+      } else {
         octopus.position["z"] -= stepPosition;
       }
       break;
@@ -166,22 +255,36 @@ function onKeyDown(e) {
       break;
     case 50: //camera 2
       camera.position["x"] = 0;
-      camera.position["y"] = 20;
+      camera.position["y"] = 30;
       camera.position["z"] = 0;
+      break;
+    case 51: //camera 3
+      camera.position["x"] = 20;
+      camera.position["y"] = 0;
+      camera.position["z"] = 0;
+      break;
+    case 52: //camera 4
+      camera.position["x"] = -15;
+      camera.position["y"] = 5;
+      camera.position["z"] = 15;
+      break;
+    case 53: //camera 5
+      camera.position["x"] = 0;
+      camera.position["y"] = 5;
+      camera.position["z"] = -30;
       break;
     // Aumentar tamanho do objeto
     case 107:
     case 187:
-      if(object == 0){
-        if (spiderman.position["x"] > 5) {
+      if (object == 0) {
+        if (hulkAbomination.position["x"] > 5) {
           alert("Voce está no tamanho máximo");
         } else {
-          spiderman.position["x"] += scaleResize.x;
-          spiderman.position["y"] += scaleResize.y;
-          spiderman.position["z"] += scaleResize.z;
+          hulkAbomination.position["x"] += scaleResize.x;
+          hulkAbomination.position["y"] += scaleResize.y;
+          hulkAbomination.position["z"] += scaleResize.z;
         }
-      }
-      else{
+      } else {
         if (octopus.position["x"] > 5) {
           alert("Voce está no tamanho máximo");
         } else {
@@ -194,16 +297,15 @@ function onKeyDown(e) {
     //Diminuir tamanho do objeto
     case 109:
     case 189:
-      if(object == 0){
-        if (spiderman.position["x"] <= 0) {
+      if (object == 0) {
+        if (hulkAbomination.position["x"] <= 0) {
           alert("Voce está no tamanho mínimo");
         } else {
-          spiderman.position["x"] -= scaleResize.x;
-          spiderman.position["y"] -= scaleResize.y;
-          spiderman.position["z"] -= scaleResize.z;
+          hulkAbomination.position["x"] -= scaleResize.x;
+          hulkAbomination.position["y"] -= scaleResize.y;
+          hulkAbomination.position["z"] -= scaleResize.z;
         }
-      }
-      else{
+      } else {
         if (octopus.position["x"] <= 0) {
           alert("Voce está no tamanho mínimo");
         } else {
@@ -218,35 +320,33 @@ function onKeyDown(e) {
       openImg();
       break;
     case 82:
-      raster()
+      raster();
       break;
   }
 }
-function raster(){
+function raster() {
+  var base_url = "http://localhost:40983/";
 
-  var base_url = 'http://localhost:8888/pp3-pg/'
+  var array = [];
 
-  var array = []
-
-  var file = loadFile(base_url + 'assets/3D/base_spider_man/base_spider_man.obj')
-  file = file.split('\n')
-  for(i = 0; i < file.length; i++){
-    if(file[i].split(' ')[0].length == 1){
-      var split = file[i].split(' ')
+  var file = loadFile("assets/3D/base_spider_man/base_spider_man.obj");
+  file = file.split("\n");
+  for (i = 0; i < file.length; i++) {
+    if (file[i].split(" ")[0].length == 1) {
+      var split = file[i].split(" ");
       var aux = {
-        x: Math.round((split[1]*100)),
-        y: Math.round((split[2]*100)),
-      }
-      array.push(aux)
+        x: Math.round(split[1] * 100),
+        y: Math.round(split[2] * 100),
+      };
+      array.push(aux);
     }
   }
   file = undefined;
-  array = array.filter((obj, index, self) =>
-    index === self.findIndex((t) => (
-      t.x === obj.x && t.y === obj.y
-    ))
-  )
-  console.log(array)
+  array = array.filter(
+    (obj, index, self) =>
+      index === self.findIndex((t) => t.x === obj.x && t.y === obj.y)
+  );
+  console.log(array);
 
   var canvas = document.createElement("canvas");
   canvas.setAttribute("width", 1000);
@@ -257,12 +357,13 @@ function raster(){
   //Then you can draw a point at (10,10) like this:
 
   var ctx = canvas.getContext("2d");
-  for(i = 0; i < array.length; i++){
-    ctx.fillRect(500-array[i].x,(500-array[i].y),10,10);
+  for (i = 0; i < array.length; i++) {
+    ctx.fillRect(500 - array[i].x, 500 - array[i].y, 10, 10);
   }
 
   const dataUrl = canvas.toDataURL("png");
-  var iframe = "<iframe width='100%' height='100%' src='" + dataUrl + "'></iframe>"
+  var iframe =
+    "<iframe width='100%' height='100%' src='" + dataUrl + "'></iframe>";
   console.log(dataUrl);
   const win = window.open();
   win.document.open();
@@ -273,8 +374,33 @@ function loadFile(filePath) {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.open("GET", filePath, false);
   xmlhttp.send();
-  if (xmlhttp.status==200) {
+  if (xmlhttp.status == 200) {
     result = xmlhttp.responseText;
   }
   return result;
+}
+
+function vertexShader() {
+  return `
+    varying vec3 vUv; 
+
+    void main() {
+      vUv = position; 
+
+      vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
+      gl_Position = projectionMatrix * modelViewPosition; 
+    }
+  `;
+}
+
+function fragmentShader() {
+  return `
+      uniform vec3 colorA; 
+      uniform vec3 colorB; 
+      varying vec3 vUv;
+
+      void main() {
+        gl_FragColor = vec4(mix(colorA, colorB, vUv.z), 1.0);
+      }
+  `;
 }
